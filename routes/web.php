f062e1ca58d520b3b5d6b724257fe59f3706e704
joshtheone <joshtheone70@gmail.com>
2023-04-11 17:39:33 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
-use App\Controllers\testController;
 use Rahisi\Routes\Route;
 
-Route::Get("", [testController::class,'index']);
-Route::Post("newTest", [testController::class,'store']);
-Route::Get("putTest", [testController::class,'put']);
-Route::Get("deleteTest", [testController::class,'delete']);
+Route::Get("", function ()
+{
+  view("index");
+});
